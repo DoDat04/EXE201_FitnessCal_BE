29a0783e55266729078f1c88b5e20551a1b7cdd3
Lam Tran <lamtvtse182432@fpt.edu.vn>
2025-09-04 20:38:39 +0700

    change sha256 hash to bcrypt hash, validate discord login json

---
+++ FitnessCal.BLL/DTO/AuthDTO/Request/DiscordLoginRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCal.BLL.DTO.AuthDTO.Request;
+
+public class DiscordLoginRequestDTO
+{
+    [Required]
+    public string Uid { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+}
