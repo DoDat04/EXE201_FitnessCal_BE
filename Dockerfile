232da42915b2e69e5dee08b8ac8c603d51065df3
Minhngo04 <minhnqse182368@fpt.edu.vn>
2025-10-09 14:45:51 +0700

    fix program worker

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+﻿FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+EXPOSE 8081
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["FitnessCal.API/FitnessCal.API.csproj", "FitnessCal.API/"]
+COPY ["FitnessCal.BLL/FitnessCal.BLL.csproj", "FitnessCal.BLL/"]
+COPY ["FitnessCal.Domain/FitnessCal.Domain.csproj", "FitnessCal.Domain/"]
+COPY ["FitnessCal.DAL/FitnessCal.DAL.csproj", "FitnessCal.DAL/"]
+RUN dotnet restore "FitnessCal.API/FitnessCal.API.csproj"
+COPY . .
+WORKDIR "/src/FitnessCal.API"
+RUN dotnet build "./FitnessCal.API.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./FitnessCal.API.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "FitnessCal.API.dll"]
--- FitnessCal.Worker/Program.cs
+++ FitnessCal.Worker/Program.cs
@@ -9,6 +9,7 @@ using FitnessCal.Worker.Define;
 using FitnessCal.Worker.Implement;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using Scrutor;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -20,11 +21,38 @@ builder.Logging.AddConsole();
 // -------------------- DbContext --------------------
 builder.Services.AddDbContext<FitnessCalContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        npgsqlOptions =>
+        {
+            // 👉 Tự retry khi gặp lỗi mạng hoặc Supabase sleep
+            npgsqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(10),
+                errorCodesToAdd: null
+            );
+
+            // 👉 Cho phép lệnh chạy tối đa 60 giây (mặc định chỉ 15 giây)
+            npgsqlOptions.CommandTimeout(60);
+        });
 });
 
-// -------------------- HttpClient --------------------
+
+// -------------------- MongoDB --------------------
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("MongoConnection");
+    return new MongoClient(connectionString);
+});
+
+builder.Services.AddSingleton<IMongoDatabase>(sp =>
+{
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase("FitnessCalDB"); // ⚠️ Đặt tên DB thật
+});
+
+// -------------------- HttpClient + HttpContext --------------------
 builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
 
 // -------------------- Scan Repository & Service --------------------
 builder.Services.Scan(scan => scan
